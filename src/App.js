f8ab7f5f4fc83a8477443560ad36899a4d5be058
Davi Antunes <davigurgel2@gmail.com>
2018-03-05 21:29:39 -0300

    Mudança de categoria adcionada sem a API

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ class BooksApp extends React.Component {
         "id":"toKillAMockingbird",
         "title":"To Kill a Mockingbird",
         "authors":"Harper Lee",
-        "coverURL":"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        "shelf":"null"
+        "imageLinks":"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        "shelf":"currentlyReading"
       },
       {
         "id":"endersGame",
         "title":"Ender's Game",
         "authors":"Orson Scott Card",
-        "coverURL":"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        "shelf":"null"
+        "imageLinks":"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+        "shelf":"currentlyReading"
       },
       {
         "id":"1776",
         "title":"1776",
         "authors":"David McCullough",
-        "coverURL":"http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        "shelf":"null"
+        "imageLinks":"http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+        "shelf":"wantToRead"
       }
 
     ],
@@ -37,6 +37,27 @@ class BooksApp extends React.Component {
       this.setState({books:books})
     })
   }
+  removeBook = (book) => {
+    this.setState(state=>({
+      books: state.books.filter((b) => b.id !== book.id)
+    }))
+  }
+  moveBook = (book,shelf) => {
+    this.setState(state=>({
+      books: state.books.filter((b) => b.id !== book.id)
+    }))
+    this.setState((state)=>({
+      books: state.books.concat([
+        {
+          "id":book.id,
+          "title":book.title,
+          "authors":book.authors,
+          "imageLinks":book.imageLinks,
+          "shelf":shelf
+        }
+      ])
+    }))
+  }
 
   render() {
     return (
@@ -46,9 +67,9 @@ class BooksApp extends React.Component {
             <h1>MyReads</h1>
           </div>
           <div className="list-books-content">
-            <Bookshelf myBooks={this.state.books} shelf={'wantToRead'}/>
-            <Bookshelf myBooks={this.state.books} shelf={'currentlyReading'}/>
-            <Bookshelf myBooks={this.state.books} shelf={'doneReading'}/>
+            <Bookshelf onMoveBook={this.moveBook} myBooks={this.state.books.filter((book)=>book.shelf==='currentlyReading')} shelf={'currentlyReading'}/>
+            <Bookshelf onMoveBook={this.moveBook} myBooks={this.state.books.filter((book)=>book.shelf==='wantToRead')} shelf={'wantToRead'}/>
+            <Bookshelf onMoveBook={this.moveBook} myBooks={this.state.books.filter((book)=>book.shelf==='read')} shelf={'Read'}/>
           </div>
         </div>
       </div>
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react'
+//title
+//authors
+//publisher
+//publishedDate
+//description
+//industryIdentifiers
+//readingModes
+//pageCount
+//printType
+//categories
+//averageRating
+//ratingsCount
+//maturityRating
+//allowAnonLogging
+//contentVersion
+//imageLinks
+//languagepreviewLink
+//infoLink
+//canonicalVolumeLink
+//id
+//shelf
 class Bookshelf extends Component {
   state = {
     myBooks:[]
   }
   render(){
+    this.props.myBooks.map((book)=>(
+      console.log(book.imageLinks)
+    ))
     return(
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.shelf}</h2>
@@ -14,21 +38,21 @@ class Bookshelf extends Component {
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{
-                      width: 128, height: 193, backgroundImage:`url(${book.coverURL})`
+                      width: 128, height: 193, backgroundImage:`url(${book.imageLinks})`
                       }}>
                     </div>
                     <div className="book-shelf-changer">
                       <select>
                       <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
+                      <option onClick={()=>this.props.onMoveBook(book,"currentlyReading")}value="currentlyReading">Currently Reading</option>
+                      <option onClick={()=>this.props.onMoveBook(book,"wantToRead")}value="wantToRead">Want to Read</option>
+                      <option onClick={()=>this.props.onMoveBook(book,"read")} value="read">Read</option>
                       <option value="none">None</option>
                       </select>
                     </div>
                   </div>
                  <div className="book-title">{book.title}</div>
-                 <div className="book-authors">{book.author}</div>
+                 <div className="book-authors">{book.authors}</div>
                 </div>
               </li>
             ))}
